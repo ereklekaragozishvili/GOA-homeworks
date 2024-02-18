f0f1dfd8b94171c415156351141aa4933b4a9097
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-02-18 18:24:27 +0400

    commit

---
+++ day 6/day6.py
@@ -0,0 +1,10 @@
+name = input("whats your name: ")
+lastname = input ("whats your lastname: ")
+age = input("whats your age: ")
+
+
+
+
+
+introduction = f'hello i am {name} my last name is {lastname} my age {age}'
+print(introduction)
