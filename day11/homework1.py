3098c9b628023bc64d2399b7a78e32c633ee569b
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-03-03 01:27:45 +0400

    commit

--- day11/homework1.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Enter a number"))
-
-if number < 0:
-    print("the number is negative")
-elif number > 0:
-    print("number is positive")
-elif number == 0:
-    print("number is zero")
