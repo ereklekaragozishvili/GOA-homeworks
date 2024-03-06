6a4e6f18f44baa0a65d7378a5d7c2576cd73926c
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-03-07 00:50:36 +0400

    commit

--- day 02/day2.py
+++ day 02/day2.py
@@ -1,1 +1,3 @@
-print("day 0 homework")
+print("gavicani, sandro zabakhidze, saba ratiani, saba shalikashvili,vato argutashvili data fofxadze,dachi sazandrishvili, barbare tinikasvili,nika shavadze")
+
+print("goa  shemosvlitanave shemiyvarda radgan bevri megobrebi gavichine da kidev ufro imit shemiyvarda rom aq yvela yvelas exmareba da megobruli garemoa chemi azrit ki goa yvelaze magaribakademiaa mtel msoflioshi")
