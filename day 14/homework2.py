0fee6d94ee45fd3b7003bb58d500a6525b23d90a
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-03-07 21:48:32 +0400

    commit

---
+++ day 14/homework2.py
@@ -0,0 +1,4 @@
+numbers_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+even_numbers_list = [num for num in numbers_list if num % 2 == 0]
+print("Even numbers list:", even_numbers_list)
---
+++ day 14/homework3.py
@@ -0,0 +1,2 @@
+new_list = [num for num in range(50, 101)]
+print("Part of the list with negative indexes:", new_list[-10:])
