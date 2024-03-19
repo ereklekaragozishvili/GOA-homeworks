0d91e70e192103f70f52d04187403582e8768416
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-03-20 01:09:33 +0400

    commit

---
+++ classwork/00.py
@@ -0,0 +1,9 @@
+def funk(erekle):
+    for i range(count):
+    print ("erekle")
+
+    funk(8)
+
+    funk(2)
+
+    funk(10)
---
+++ day 12/homework4.py
@@ -0,0 +1,3 @@
+for num in range(1, 21):
+    if num % 6 == 0:
+        print(num)
